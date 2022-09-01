45b0088f52430f5424f31d830dfdae6452bf4f95
NSD <73041332+Nisedo@users.noreply.github.com>
2022-09-01 17:28:30 +0000

    Can't figure it out

--- script.js
+++ script.js
@@ -12,36 +12,54 @@ const getComputerChoice= () => {
       }
   // console.log(getComputerChoice());
 
+  let scorePlayer1 = 0;
+  let scorePlayer2 = 0;
+
   function playRound(playerSelection, computerSelection){
+    
     player1 = prompt("Rock, Paper or Scissors ?: ").toUpperCase();
+    console.log("Player 1 choose:", player1)
+    console.log(typeof player1)
+    while (player1 != "ROCK" || player1 != "PAPER" || player1 != "SCISSORS"){
+        prompt("Rock, Paper or Scissors ?: ").toUpperCase();
+    }
+
     player2 = getComputerChoice();
-    console.log(player2)
-    if (player1 == "ROCK" && player2 == "Scissors")
+    console.log("Computer choose:", player2)
+
+    if (player1 == "ROCK" && player2 == "Scissors"){
         console.log("You Win! Rock beats Scissors");
-    else if (player1 == "ROCK" && player2 == "Rock")
+        return scorePlayer1++;
+    } else if (player1 == "ROCK" && player2 == "Rock"){
         console.log("It's a tie");
-    else if (player1 == "ROCK" && player2 == "Paper")
-        console.log("You Lose! Paper beats Rock");
-    else if (player1 == "PAPER" && player2 == "Scissors")
-        console.log("You Lose! Scissors beats Paper");
-    else if (player1 == "PAPER" && player2 == "Paper")
+    } else if (player1 == "ROCK" && player2 == "Paper"){
+        console.log("You Lose! Paper beats Rock")
+        return(scorePlayer2++);
+    } else if (player1 == "PAPER" && player2 == "Scissors"){
+        console.log("You Lose! Scissors beats Paper")
+        return(scorePlayer2++);
+    } else if (player1 == "PAPER" && player2 == "Paper"){
         console.log("It's a tie");
-    else if (player1 == "PAPER" && player2 == "Rock")
-        console.log("You Win! Rock beats Rock");
-    else if (player1 == "SCISSORS" && player2 == "Scissors")
+    } else if (player1 == "PAPER" && player2 == "Rock"){
+        console.log("You Win! Paper beats Rock")
+        return scorePlayer1++;
+    } else if (player1 == "SCISSORS" && player2 == "Scissors"){
         console.log("It's a tie");
-    else if (player1 == "SCISSORS" && player2 == "Paper")
-        console.log("You Win! Rock beats Paper");
-    else if (player1 == "SCISSORS" && player2 == "Rock")
+    } else if (player1 == "SCISSORS" && player2 == "Paper"){
+        console.log("You Win! Scissors beats Paper")
+        return(scorePlayer1++);
+    } else if (player1 == "SCISSORS" && player2 == "Rock"){
         console.log("You Lose! Scissors beats Rock");
-        
-  }
+        return(scorePlayer2++);
+        }
+    }
 
-  playRound()
+  // playRound();
 
-  function game(){
-    playRound()
-    for (let i = 0; i < 5; i++) {
-        // your code here!
-     }
-  }
+  function game(){  
+  for (let i = 0; i < 5; i++) {
+            playRound();
+            console.log("Score Player 1: ", scorePlayer1, "Score Computer: ", scorePlayer2);
+        }
+    }
+    game();
