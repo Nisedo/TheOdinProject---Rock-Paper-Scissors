0be66d859738ce34ef76e0306c24dd7a6eeb766c
NSD <73041332+Nisedo@users.noreply.github.com>
2022-09-04 21:27:58 +0000

    Solved the game function bug

--- script.js
+++ script.js
@@ -10,56 +10,73 @@ const getComputerChoice= () => {
     return 'Scissors';
     }
       }
+
   // console.log(getComputerChoice());
 
-  let scorePlayer1 = 0;
-  let scorePlayer2 = 0;
+  let scorePlayer = 0;
+  let scoreComputer = 0;
+  let ties = 0;
 
   function playRound(playerSelection, computerSelection){
-    
-    player1 = prompt("Rock, Paper or Scissors ?: ").toUpperCase();
-    console.log("Player 1 choose:", player1)
-    console.log(typeof player1)
-    while (player1 != "ROCK" || player1 != "PAPER" || player1 != "SCISSORS"){
-        prompt("Rock, Paper or Scissors ?: ").toUpperCase();
-    }
-
-    player2 = getComputerChoice();
-    console.log("Computer choose:", player2)
-
-    if (player1 == "ROCK" && player2 == "Scissors"){
+    computerSelection = getComputerChoice();
+    console.log("Computer choose:", computerSelection)
+    if (playerSelection == "ROCK" && computerSelection == "Scissors"){
         console.log("You Win! Rock beats Scissors");
-        return scorePlayer1++;
-    } else if (player1 == "ROCK" && player2 == "Rock"){
+        return scorePlayer++;
+    } else if (playerSelection == "ROCK" && computerSelection == "Rock"){
         console.log("It's a tie");
-    } else if (player1 == "ROCK" && player2 == "Paper"){
+    } else if (playerSelection == "ROCK" && computerSelection == "Paper"){
         console.log("You Lose! Paper beats Rock")
-        return(scorePlayer2++);
-    } else if (player1 == "PAPER" && player2 == "Scissors"){
+        return scoreComputer++;
+    } else if (playerSelection == "PAPER" && computerSelection == "Scissors"){
         console.log("You Lose! Scissors beats Paper")
-        return(scorePlayer2++);
-    } else if (player1 == "PAPER" && player2 == "Paper"){
+        return scoreComputer++;
+    } else if (playerSelection == "PAPER" && computerSelection == "Paper"){
         console.log("It's a tie");
-    } else if (player1 == "PAPER" && player2 == "Rock"){
+    } else if (playerSelection == "PAPER" && computerSelection == "Rock"){
         console.log("You Win! Paper beats Rock")
-        return scorePlayer1++;
-    } else if (player1 == "SCISSORS" && player2 == "Scissors"){
+        return scorePlayer++;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "Scissors"){
         console.log("It's a tie");
-    } else if (player1 == "SCISSORS" && player2 == "Paper"){
+    } else if (playerSelection == "SCISSORS" && computerSelection == "Paper"){
         console.log("You Win! Scissors beats Paper")
-        return(scorePlayer1++);
-    } else if (player1 == "SCISSORS" && player2 == "Rock"){
+        return scorePlayer++;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "Rock"){
         console.log("You Lose! Scissors beats Rock");
-        return(scorePlayer2++);
+        return scoreComputer++;
         }
     }
 
-  // playRound();
-
-  function game(){  
-  for (let i = 0; i < 5; i++) {
-            playRound();
-            console.log("Score Player 1: ", scorePlayer1, "Score Computer: ", scorePlayer2);
+  function game(){
+    for (let i = 1; i < 6; i++){
+        playerSelection = prompt(`Type Rock, Paper, or Scissors:`).toUpperCase();
+        playRound(playerSelection, getComputerChoice)
+        // console.log("You choose: " + playerSelection.toUpperCase());
+        // console.log(`Game: ` + i);
+        console.log("Game: " + i + '\n' + "Your Score: "+ scorePlayer + '\n' + "Computer Score: " + scoreComputer);
+        if (i == 5){
+            if (scorePlayer > scoreComputer){
+                console.log("Congrats, you win !")
+            } else if (scorePlayer < scoreComputer){
+                console.log("Too bad, you loose !")
+            } else {
+                console.log("It's a tie !")
+            }
+            }
         }
-    }
-    game();
+}
+
+game();
+/* 
+                                        function game(){
+                                            for (let i = 1; i < 4; i++){
+                                                choice = prompt(`Type Rock, Paper, or Scissors:`);
+                                                alert(`You: `+ choice[0].toUpperCase() + choice.substring(1) + `!\r` + playerSelection());
+                                                alert(`Game: ` + i + `/3\r` + `Score:\rYou: ` + w +`\rCPU: ` + l + `\rTies: ` + t);
+                                                if (badAnswer == 1){
+                                                    i--;
+                                                    badAnswer--;
+                                                }
+                                        }}
+
+                                        game(); */
